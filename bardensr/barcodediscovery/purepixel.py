f08d68809455734b204e83d95f9143e253a34978
Ubuntu <ubuntu@ip-172-31-75-248.ec2.internal>
2021-04-20 23:10:38 +0000

    added channel numbers for convert_codebook_to_onehot_form

--- bardensr/barcodediscovery/purepixel.py
+++ bardensr/barcodediscovery/purepixel.py
@@ -23,9 +23,8 @@ def seek_barcodes(X,thre_onehot,proportion_good_rounds_required):
         # deduplicat
         channelchoicesfl=channelchoices.reshape((R,-1))
         barcodes_found=np.unique(channelchoicesfl[:,good],axis=-1).T
-
         # convert to a normal codebook form
-        barcodes_found=misc.convert_codebook_to_onehot_form(barcodes_found)
+        barcodes_found=misc.convert_codebook_to_onehot_form(barcodes_found, C=C)
 
         return barcodes_found
 
--- bardensr/misc.py
+++ bardensr/misc.py
@@ -92,13 +92,14 @@ def maybe_tqdm(n,use_tqdm_notebook,*args,**kwargs):
     else:
         return n
 
-def convert_codebook_to_onehot_form(codebook):
+def convert_codebook_to_onehot_form(codebook, C=None):
     '''
     Input: codebook, JxR
     Output: codebook, RxCxJ
     '''
     J,R=codebook.shape
-    C=codebook.max()+1
+    if C == None:
+        C=codebook.max()+1
     codes=np.eye(C,dtype=np.float)
     codes=np.concatenate([codes,np.full((1,C),np.nan)],axis=0)
     codebook=codes[codebook.ravel()].reshape((J,R,C))
